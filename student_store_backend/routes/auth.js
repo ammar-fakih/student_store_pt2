f292b8543dd567019f1bde3629817e94c5ab880f
Ammar Fakih <ammar.fakih02@gmail.com>
2022-07-11 16:25:20 -0700

    step 5

--- student_store_backend/routes/auth.js
+++ student_store_backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
-const Order = require("../models/orders")
+const Order = require('../models/orders');
 const { createUserJwt } = require('../utils/tokens');
 const security = require('../middleware/security');
 const router = express.Router();
@@ -29,8 +29,9 @@ router.get('/me', security.requireAuthenticatedUser, async (req, res, next) => {
   try {
     const { email } = res.locals.user;
     const user = await User.fetchUserByEmail(email);
+    const orders = await Order.listOrdersForUser(user.id);
     const publicUser = User.makePublicUser(user);
-    return res.status(200).json({ user: publicUser });
+    return res.status(200).json({ user: publicUser, orders });
   } catch (e) {
     next(e);
   }
